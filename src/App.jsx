cb5ed6098672cea34a223a698a524055686012d0
Yanis1717 <yanis.laid@etu.univ-st-etienne.fr>
2026-05-21 10:25:34 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
       <h1>🚀 Déploiement automatique opérationnel</h1>
       <p>Projet réalisé par <strong>Yanis LAÏD</strong></p>
       <p>Future application : gestion de tâches Kanban</p>
+      <p> Test </p>
     </div>
   );
 }
